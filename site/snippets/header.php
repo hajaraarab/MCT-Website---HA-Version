75665b72050290a01bdb7e5e3708e62ebf2b0e38
hajaraarab <hajar.aarab@student.kdg.be>
2025-10-13 20:45:09 +0200

    Paginastructuur en navigatiemenu

---
+++ site/snippets/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php if($page->isHomePage()): ?>
+        <title><?= $site->title() ?></title>
+    <?php else: ?>
+        <title><?= $site->seoTitle()->or($page->title()) ?> / <?= $site->title() ?></title>
+    <?php endif ?>
+
+    <?= css('assets/css/index.css') ?>
+    <?= css('@auto') ?>
+</head>
+<body>
+
+    <header class="header">
+        <a class="logo" href="<?= $site->url() ?>"><?= $site->title() ?></a>
+        <?php snippet('menu') ?>
+    </header>
