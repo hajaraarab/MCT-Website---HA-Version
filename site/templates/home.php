49df39ed6f8a8212d76cdb839bb80a85c5c49f07
hajaraarab <hajar.aarab@student.kdg.be>
2025-10-18 14:09:40 +0200

    Nieuwe About-sectie en laatste sociale media post-sectie toegevoegd aan startpagina

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,15 +1,37 @@
 <?php
-    $heroImage = $page->image($page->hero()); 
+    $heroImage = $page->hero()->toFile(); 
+
+    $aboutPage = $site->find('about'); 
+
+    $latestPostsTitle = $page->latestPostsTitle(); 
+    $latestPostsText = $page->latestPostsText(); 
 ?>
 
 <?php snippet('header') ?>
 
 <div class="container">
 
-    <div class="herosection">
+    <div class="hero-section">
         <?php if($heroImage): ?>
             <img src="<?= $heroImage->url() ?>" alt="<?= $heroImage->alt() ?>">
         <?php endif ?>
+
+        <h1 class="heroTitle"><?= $page->heroTitle() ?></h1>
+    </div>
+
+    <div class="about-and-posts">
+        <div class="about-preview">
+            <h1><?= $aboutPage->title() ?></h1>
+            <p><?= $aboutPage->previewText() ?></p>
+        </div>
+        <div class="lates-posts-preview">
+            <h1><?= $latestPostsTitle ?></h1>
+            <p><?= $latestPostsText ?></p>
+
+            <button>Follow us</button>
+
+            <div class="instagram-post"></div>
+        </div>
     </div>
 </div>
 
