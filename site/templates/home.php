1c52a9fde6c33b82ab620580673a2ab39f4a5190
hajaraarab <hajar.aarab@student.kdg.be>
2025-10-19 14:50:46 +0200

    Portfolio sectie toegevoegd aan de startpagina

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,6 +9,7 @@
 
     $profiles = $page->profileLinks()->toStructure();
     $courses = $page->yearOverview()->toStructure(); 
+    $portfolios = $page->portfolioPreview()->toStructure(); 
 
     $schoolLogo = $page->schoolLogo()->toFile();
     
@@ -98,7 +99,29 @@
             <p><?= $page->enrollText() ?></p>
             <button>Enroll</button>
         </div>
-        
+    </div>
+
+    <div class="portfolios-preview">
+        <h3><?= $page->portfolioTitle() ?></h3>
+        <p><?= $page->portfolioIntro() ?></p>
+
+        <?php foreach($portfolios as $portfolio): ?>
+            <div class="portfolio-card">
+
+                <?php $portfolioPicture = $portfolio->image()->toFile(); ?>
+                <?php if($portfolioPicture): ?>
+                    <img src="<?= $portfolioPicture->url() ?>">
+                <?php endif; ?>
+
+                <div class="porfolio-info">
+                    <h4><?= $portfolio->portfolioName() ?></h4>
+                    <p><?= $portfolio->portfolioInfo() ?></p>
+                    <p><?= $portfolio->portfolioAuthor() ?></p>
+                </div>
+            </div>
+        <?php endforeach ?>
+
+        <button>See more</button>
     </div>
 </div>
 
